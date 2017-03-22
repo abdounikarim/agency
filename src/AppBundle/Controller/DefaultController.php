6a5d5800c6127f4fce6afb57e0e9b21ed60865ba
abdounikarim <abdounikarim@gmail.com>
2017-03-22 22:21:06 +0100

    add curriculum vitae pdf

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,5 +15,37 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         return $this->render('default/index.html.twig');
+    }
+    /**
+     * @Route("/cv", name="cv")
+     */
+    public function cvAction()
+    {
+        return $this->render('default/cv.html.twig');
+    }
+    /**
+     * @Route("/cvpdf", name="cvpdf")
+     */
+    public function cvpdfAction(){
+        $html = $this->renderView('cv/cvpdf.html.twig');
+
+        $filename = sprintf('cv.pdf');
+
+        $options = [
+            'margin-top'    => 0,
+            'margin-right'  => 0,
+            'margin-bottom' => 0,
+            'margin-left'   => 0,
+        ];
+
+        return new Response(
+
+            $this->get('knp_snappy.pdf')->getOutputFromHtml($html, $options),
+            200,
+            [
+                'Content-Type'        => 'application/pdf',
+                'Content-Disposition' => sprintf('attachment; filename="%s"', $filename),
+            ]
+        );
     }
 }
